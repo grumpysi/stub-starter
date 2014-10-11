7a9191dac2684cb07f2bbe9b58fe17c66e27ed3a
Simon Nicklin <simon@njo.im>
2014-10-11 23:47:12 +0100

    Added new Starter Kit system to choose your initial template app.

---
+++ kits/simple/001/public/index.php
@@ -0,0 +1,56 @@
+<?php
+
+// define paths
+define('APP_ROOT_PATH', __DIR__);
+define('APP_CONFIG_PATH', __DIR__.'/../app/config');
+define('APP_VIEW_PATH', __DIR__.'/../app/views');
+define('APP_PATH', __DIR__.'/../app');
+
+// setup auto loader
+require_once APP_ROOT_PATH.'/../vendor/autoload.php';
+
+use Symfony\Component\Yaml\Parser;
+$yaml = new Parser();
+
+$app = new Silex\Application();
+
+// Load general config
+$config = $yaml->parse(file_get_contents(APP_CONFIG_PATH.'/config.yml'));
+if (isset($config['app'])) {
+    $app['debug'] = isset($config['app']['debug']) ? $config['app']['debug'] : false;
+}
+
+// Configure Twig
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => APP_VIEW_PATH
+));
+
+// Register simple routes
+$routing = $yaml->parse(file_get_contents(APP_PATH.'/routing.yml'));
+if (isset($routing['routings'])) {
+    foreach($routing['routings'] as $routing) {
+        $method = isset($routing['method']) ? $routing['method'] : 'get';
+        $app->get($routing['url'], function () use ($app, $routing) {
+            return $app['twig']->render($routing['view']);
+        });
+    }
+}
+
+// Register complex routes
+require_once APP_PATH.'/routes.php';
+
+// Handle errors
+$app->error(function (\Exception $e, $code) use ($app) {
+    switch ($code) {
+        case 404:
+            $message = $app['twig']->render('layouts/404.twig');
+            break;
+        default:
+           // $message = $app['twig']->render('layouts/500.twig');
+    }
+ //   return new Response($message, $code);
+    return $message;
+});
+
+// Start the application
+$app->run();
